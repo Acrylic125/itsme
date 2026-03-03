72243c3415e2db5161412d26db7442b886c71c05
Acrylic125 <acrylic125email@gmail.com>
2026-03-03 09:11:04 +0800

    chore: add tf

---
+++ infra/providers.tf
@@ -0,0 +1,4 @@
+provider "cloudflare" {
+  # Provide as an environment variable, CLOUDFLARE_API_TOKEN
+}
+
---
+++ infra/variables.tf
@@ -0,0 +1,7 @@
+
+variable "account_id" {
+  description = "Cloudflare Account ID"
+  type        = string
+  sensitive   = true
+}
+
