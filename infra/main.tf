ea97ee35e7c2b7804287f02b65f785af9b675030
Acrylic125 <acrylic125email@gmail.com>
2026-03-03 12:38:28 +0800

    chore: add r2 and d1

--- infra/main.tf
+++ infra/main.tf
@@ -7,3 +7,15 @@ terraform {
   }
 }
 
+resource "cloudflare_d1_database" "itsme-database" {
+  account_id = var.account_id
+  name       = "itsme"
+  read_replication = {
+    mode = "disabled"
+  }
+}
+
+resource "cloudflare_r2_bucket" "itsme-bucket" {
+  account_id = var.account_id
+  name       = "itsme-bucket"
+}
